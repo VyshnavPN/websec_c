98387c11df987ba594cf00c03ebf083ff4e5fed1
dron <vyshnav.njattuketty@gmail.com>
2026-02-27 09:57:47 +0530

    debug hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 // src/components/Hero.jsx
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
 import CyberScene from '../canvas/CyberScene'
 import InfoPanel from '../ui/InfoPanel'
 
@@ -8,7 +9,13 @@ export default function Hero() {
     <>
       {/* Background 3D Object - Centered */}
       <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
-        <CyberScene />
+        {/* Canvas must wrap any react-three/fiber scene component */}
+        <Canvas camera={{ position: [0, 0, 6], fov: 45 }}>
+          <ambientLight intensity={0.4} />
+          <pointLight position={[10, 10, 10]} intensity={1.5} />
+          <spotLight position={[-10, 10, 10]} angle={0.15} penumbra={1} />
+          <CyberScene />
+        </Canvas>
       </div>
 
       {/* Foreground UI Elements */}
